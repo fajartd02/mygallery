dcfdeba7538340b3e5b60232df71d32de6e064ca
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-01-03 03:35:15 +0800

    create user entity

---
+++ core/entity/user.go
@@ -0,0 +1,30 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Memories []Memory `json:"Memories"`
+	FullName string   `json:"fullName"`
+	Email    string   `json:"email"`
+	Password string   `json:"password"`
+}
+
+type UserInput struct {
+	FullName string `json:"fullName"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	Email       string `json:"email"`
+	UserID      uint   `json:"userID"`
+	TokenString string `json:"token"`
+}
