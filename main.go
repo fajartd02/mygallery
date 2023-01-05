e9cd89643bfc4eb4b5ba3ec068bacae2ade6725d
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-01-05 17:03:38 +0800

    all delete unnceserary code

--- main.go
+++ main.go
@@ -5,12 +5,8 @@ import (
 	"github.com/fajartd02/mygallery/core/entity"
 	"github.com/fajartd02/mygallery/core/module"
 	"github.com/fajartd02/mygallery/handler"
-	commentrepository "github.com/fajartd02/mygallery/repository/comment"
-	filerepository "github.com/fajartd02/mygallery/repository/file"
 	memoryrepository "github.com/fajartd02/mygallery/repository/memory"
-	reportrepository "github.com/fajartd02/mygallery/repository/report"
 	userrepository "github.com/fajartd02/mygallery/repository/user"
-	uservoterepository "github.com/fajartd02/mygallery/repository/user-vote"
 	"github.com/fajartd02/mygallery/routes"
 )
 
@@ -18,30 +14,18 @@ func main() {
 
 	db := config.InitDB()
 	// cfg := config.InitConfig()
-	db.AutoMigrate(&entity.User{}, &entity.Memory{}, &entity.Report{}, &entity.UserVote{}, &entity.Comment{}, &entity.File{})
+	db.AutoMigrate(&entity.User{}, &entity.Memory{})
 
 	userRepo := userrepository.New()
-	commentRepo := commentrepository.New()
-	fileRepo := filerepository.New()
-	reportRepo := reportrepository.New()
-	userVoteRepo := uservoterepository.New()
 	memoryRepo := memoryrepository.New()
 
 	userUc := module.NewUserUsecase(userRepo)
 	memoryUc := module.NewMemoryUseCase(memoryRepo)
-	commentUc := module.NewCommentUsecase(commentRepo, userRepo)
-	userVoteUc := module.NewUserVoteUsecase(userVoteRepo)
-	fileUc := module.NewFileUseCase(fileRepo, reportRepo)
-	reportUc := module.NewReportUseCase(reportRepo, userRepo)
 
 	userHdl := handler.NewUserHandler(userUc)
 	memoryHdl := handler.NewMemoryHandler(memoryUc)
-	commentHdl := handler.NewCommentHandler(commentUc)
-	userVoteHdl := handler.NewUserVoteHandler(userVoteUc)
-	fileHdl := handler.NewFileHandler(fileUc)
-	reportHdl := handler.NewReportHandler(reportUc)
 
-	r := routes.SetupRoutes(db, *userHdl, *memoryHdl, *commentHdl, *fileHdl, *userVoteHdl, *reportHdl)
+	r := routes.SetupRoutes(db, *userHdl, *memoryHdl)
 
 	r.Run(":8080")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -15,10 +15,7 @@ func SetupRoutes(
 	// cfg config.Config,
 	userHdl handler.UserHandler,
 	memoryHdl handler.MemoryHandler,
-	commentHdl handler.CommentHandler,
-	fileHdl handler.FileHandler,
-	userVoteHdl handler.UserVoteHandler,
-	reportHdl handler.ReportHandler) *gin.Engine {
+) *gin.Engine {
 
 	r := gin.Default()
 	r.MaxMultipartMemory = 8 << 20 // 8 MiB
@@ -49,23 +46,7 @@ func SetupRoutes(
 
 		// File routes
 		file := apiV1.Group("/files")
-		file.POST("", fileHdl.UploadFile)
 		file.StaticFS("/static", http.Dir("images"))
-
-		// Comment routes
-		comment := apiV1.Group("/comments")
-		comment.Use(module.IsAuthorized())
-		comment.POST("/:reportId", commentHdl.Create)
-		comment.GET("/:reportId", commentHdl.GetAll)
-
-		// Report routes
-		report := apiV1.Group("/reports")
-		report.Use(module.IsAuthorized())
-		report.POST("/:reportId/votes", userVoteHdl.VotingReport)
-		report.GET("/:reportId/votes", userVoteHdl.GetVotesInReport)
-		report.GET("/:reportId", reportHdl.GetReport)
-		report.GET("", reportHdl.GetReports)
-		report.POST("", reportHdl.CreateReport)
 
 		memory := apiV1.Group("/memories")
 		memory.GET("", memoryHdl.GetAll)
